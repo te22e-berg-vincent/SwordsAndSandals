96f46d35e6e2de61418ff73d4de21ba5a0ba7bbc
Vincent Berg <vincent.berg@elev.ga.ntig.se>
2023-09-26 11:08:22 +0200

    commit intro

--- SwordsAndSandals/Program.cs
+++ SwordsAndSandals/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+int hhp = 250;
+int vhp = 250;
+
+Console.WriteLine("Hello player.");
+Console.WriteLine("Please select your name.");
+
+string Name = "";
+string vname = "uuu";
+
+Name = Console.ReadLine();
+
+Console.WriteLine($"Welcome {Name} ");
+Console.WriteLine("You will now go up agains XXXXXX in a fight the the DEATH");
+
+Random generator = new Random();
+
+while (hhp > 0 && vhp < 0)
+
+{
+
+
+
+
+}
+
+
+
+Console.ReadLine();
